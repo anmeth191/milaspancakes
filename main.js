9434b587ff8efb54d9ecafd1c68feb4a05857fea
anmeth191 <anmeth191@gmail.com>
2018-10-28 19:38:00 -0600

    added side bar to the website and also the burger menu

---
+++ main.js
@@ -0,0 +1,25 @@
+/*First this is the side nav for the respinsive design  */
+
+let sidenav = document.querySelector('#sideBar');
+let sideNavTrigger = document.querySelector('#sideNavLink');
+let fullLayout = document.querySelector('#fullLayout');
+let sideBarContent = document.querySelector('#sideBar__content');
+let sideBarClose = document.querySelector('#iconClose');
+
+
+sideNavTrigger.addEventListener('click' ,()=>{
+    sidenav.classList.toggle('sideBarShow');
+    sideBarContent.classList.toggle('sideBarContentt');
+    fullLayout.classList.toggle('fullLayoutLeft');
+    let linkSide = document.querySelectorAll('.linkSide'); 
+    linkSide.forEach(element => {element.style.display = 'block'})
+
+})
+
+sideBarClose.addEventListener('click' , () => {
+    sidenav.classList.toggle('sideBarShow');
+    sideBarContent.classList.toggle('sideBarShow');
+    fullLayout.classList.toggle('fullLayoutLeft'); 
+    let linkSide = document.querySelectorAll('.linkSide');
+   linkSide.forEach(element => {element.style.display = 'none'})
+})
